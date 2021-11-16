959e32998a7eb16493d4111a31ffad55aafa4070
Blizzard365 <72327082+Blizzard365@users.noreply.github.com>
2021-11-16 14:36:27 +0300

    Delete admin directory

--- admin/blocks/form_for_record.php
+++
@@ -1,8 +0,0 @@
-<div class="uslugi_zapic_form">
-	<form method="" action="">
-	<input class="form-control form_item  col-5" type="text" name="name" placeholder="Имя">
-	<input class="form-control form_item  col-5" type="text" name="lastname" placeholder="Фамилия">
-	<input class="form-control form_item  col-5" type="email" name="email" placeholder="E-mail">
-	<input class="form-control form_item  col-5" type="tel" name="tel" placeholder="Телефон">
-	<input class="form-control form_button  col-5" id="submit1" type="submit" name="submit" value="Записаться">
-</div>
